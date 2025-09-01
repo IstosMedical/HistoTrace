181de7cfad7b517bf52100d38be24d015a5f42e0
IstosMedical <mazhar@istos.in>
2025-09-01 11:13:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let machineCount = 0;
 const maxMachines = 10;
 
 document.getElementById("addMachineBtn").addEventListener("click", () => {
-  if (machineCount >= maxMachines) return alert("Max 7 machines allowed.");
+  if (machineCount >= maxMachines) return alert("Max 10 machines allowed.");
 
   const container = document.getElementById("machineContainer");
   const block = document.createElement("div");
@@ -13,12 +13,14 @@ document.getElementById("addMachineBtn").addEventListener("click", () => {
     </label>
     <label>Serial Number:
       <input type="text" name="serialNumber${machineCount}" required />
+      <small style="color: grey; display: block; margin-top: 6px; margin-bottom: 10px;">This is an optional field</small>
     </label>
     <label>Inside Box Details:
       <input type="text" name="boxdetails${machineCount}" />
     </label>
     <label>Notes:
       <input type="text" name="notes${machineCount}" />
+      <small style="color: grey; display: block; margin-top: 6px; margin-bottom: 10px;">Include all the important details and observations during installation</small>
     </label>
   `;
   container.appendChild(block);
