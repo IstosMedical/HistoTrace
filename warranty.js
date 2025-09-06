eee3665642a087b996d8faca192052325f7b0d73
IstosMedical <mazhar@istos.in>
2025-09-06 16:50:10 +0530

    Create warranty.js

---
+++ warranty.js
@@ -0,0 +1,18 @@
+import { createStatusCard } from "./cards.js";
+
+const spinner = document.getElementById("loadingSpinner");
+const container = document.getElementById("warrantyCards");
+
+spinner.style.display = "block";
+
+fetch("https://script.google.com/macros/s/AKfycbzbTMJTuvtsQEUHZyHfpf5u9SrgPboVptTHYrgTT41myd_c8coeb2djh2aUXaP5zF9t/exec?status=Warranty")
+  .then(res => res.json())
+  .then(data => {
+    container.innerHTML = "";
+    data.forEach(entry => container.appendChild(createStatusCard(entry)));
+  })
+  .catch(err => {
+    container.innerHTML = "<p style='color:red;'>Failed to load Warranty data.</p>";
+    console.error("Warranty fetch error:", err);
+  })
+  .finally(() => spinner.style.display = "none");
