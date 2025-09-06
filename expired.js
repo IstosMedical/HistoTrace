c3cc001963861c3a79669acfb98e31e54d5d6e45
IstosMedical <mazhar@istos.in>
2025-09-06 16:50:23 +0530

    Create expired.js

---
+++ expired.js
@@ -0,0 +1,18 @@
+import { createStatusCard } from "./cards.js";
+
+const spinner = document.getElementById("loadingSpinner");
+const container = document.getElementById("expiredCards");
+
+spinner.style.display = "block";
+
+fetch("https://script.google.com/macros/s/AKfycbzbTMJTuvtsQEUHZyHfpf5u9SrgPboVptTHYrgTT41myd_c8coeb2djh2aUXaP5zF9t/exec?status=Expired")
+  .then(res => res.json())
+  .then(data => {
+    container.innerHTML = "";
+    data.forEach(entry => container.appendChild(createStatusCard(entry)));
+  })
+  .catch(err => {
+    container.innerHTML = "<p style='color:red;'>Failed to load Expired data.</p>";
+    console.error("Expired fetch error:", err);
+  })
+  .finally(() => spinner.style.display = "none");
