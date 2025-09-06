e8a89e6bb3063fedc3a2831780a97f4ec6bcef15
IstosMedical <mazhar@istos.in>
2025-09-06 16:58:25 +0530

    Update expired.js

--- expired.js
+++ expired.js
@@ -9,10 +9,17 @@ fetch("https://script.google.com/macros/s/AKfycbzbTMJTuvtsQEUHZyHfpf5u9SrgPboVpt
   .then(res => res.json())
   .then(data => {
     container.innerHTML = "";
-    data.forEach(entry => container.appendChild(createStatusCard(entry)));
+
+    data.forEach((entry, index) => {
+      const card = createStatusCard(entry);
+      card.style.animationDelay = `${index * 100}ms`; // Staggered fade-in
+      container.appendChild(card);
+    });
   })
   .catch(err => {
     container.innerHTML = "<p style='color:red;'>Failed to load Expired data.</p>";
     console.error("Expired fetch error:", err);
   })
-  .finally(() => spinner.style.display = "none");
+  .finally(() => {
+    spinner.style.display = "none";
+  });
